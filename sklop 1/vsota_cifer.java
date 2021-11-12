808ab75a02584ce362a5bf1b51b260b08034ead6
FeriCodeDummy <94200904+FeriCodeDummy@users.noreply.github.com>
2021-11-12 23:13:35 +0100

    Create vsota_cifer.java

---
+++ sklop 1/vsota_cifer.java
@@ -0,0 +1,14 @@
+public class vsota_cifer{
+  public static void main (String [] args){
+      int vsota = 0;
+      Scanner scanner = new Scanner(System.in); // Create user input reader
+      int s = scanner.nextInt(); // Get input 
+      while (s != 0) { // move for one place in left until value is zero
+          vsota = vsota + s % 10;  // Add digit at place to sum
+          s = s / 10; // divide s by 10
+      }
+      
+      System.out.println(vsota); // Print sum
+  }
+  
+}
