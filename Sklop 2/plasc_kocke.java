5c58b18502fedb64faf32bc2b90fd97765974acd
FeriCodeDummy <94200904+FeriCodeDummy@users.noreply.github.com>
2021-11-13 20:32:35 +0100

    Create plasc_kocke.java

---
+++ Sklop 2/plasc_kocke.java
@@ -0,0 +1,17 @@
+public class plasc_kocke{
+
+  public static void main(String[] args){
+  
+    System.out.print("*" + repeat(s-2, " ") + "*" + repeat(s-2, "\n"));
+    for (int j = 0; j < 2; j++){
+        System.out.println(repeat(4, "*" + repeat(s-2, " ")) + "*" + repeat(s-3, "\n"));
+      }
+    System.out.print(repeat(s * 3 - 3 , " ") + "*" + repeat(s-2, " ") + "*");
+
+  }
+  
+    public static String repeat(int repeat, String s){
+        return new String(new char[repeat]).replace("\0", s);
+    }
+
+}
