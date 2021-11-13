35d01df8df27b46408908b79b5a555bf04947ca6
FeriCodeDummy <94200904+FeriCodeDummy@users.noreply.github.com>
2021-11-13 22:34:38 +0100

    Update plasc_kocke.java

--- Sklop 2/plasc_kocke.java
+++ Sklop 2/plasc_kocke.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class plasc_kocke{
 
   public static void main(String[] args){
   
+    Scanner scanner = new Scanner(System.in);
+    s = scanner.nextInt();
     System.out.print("*" + repeat(s-2, " ") + "*" + repeat(s-2, "\n"));
     for (int j = 0; j < 2; j++){
         System.out.println(repeat(4, "*" + repeat(s-2, " ")) + "*" + repeat(s-3, "\n"));
