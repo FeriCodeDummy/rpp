998d3588ca4e9ada23dd1604c7854687f561e074
FeriCodeDummy <94200904+FeriCodeDummy@users.noreply.github.com>
2021-11-13 20:42:42 +0100

    Create naravna.java

---
+++ Sklop 2/naravna.java
@@ -0,0 +1,12 @@
+public class naravna {
+
+  public static void main(String[] args){
+  
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int m = scanner.nextInt();
+    int k = scanner.nextInt();
+    
+  }
+  
+}
