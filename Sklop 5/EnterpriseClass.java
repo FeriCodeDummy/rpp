02ae1b59e7d5c15a465700c9847be6270d95253f
FeriCodeDummy <94200904+FeriCodeDummy@users.noreply.github.com>
2022-01-11 16:51:28 +0100

    Create EnterpriseClass.java

---
+++ Sklop 5/EnterpriseClass.java
@@ -0,0 +1,53 @@
+public class EnterpriseClass {
+
+	String name;
+	String country;
+	String subcountry;
+	String geonameid;
+	static String[][] countries;
+
+
+	public EnterpriseClass(String[] data) {
+		this.name= data[0];
+		this.country = data[1];
+		this.subcountry = data[2];
+		this.geonameid = data[3];
+	}
+
+	public static void izpis(String[] data){
+		System.out.println("Country name: " + data[1] + "\nCity: " + data[0]+ "\n\n");
+
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public String getGeonameid() {
+		return geonameid;
+	}
+
+	public String getSubcountry(){
+		return subcountry;
+	}
+
+	public void izpisivse(){
+		for (String[] data : countries){
+			izpis(data);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "EnterpriseClass{" +
+				"name='" + name + '\'' +
+				", country='" + country + '\'' +
+				", subcountry='" + subcountry + '\'' +
+				", geonameid='" + geonameid + '\'' +
+				'}';
+	}
+}
